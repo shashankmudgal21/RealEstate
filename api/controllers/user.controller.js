3c4ac77648201c90d5804f97c4beee564e44ae53
Shashank mudgal <=>
2023-11-02 17:37:51 +0530

    create getUser listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,47 +1,63 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
-import  {errorHandler}  from "../utils/error.js";
-import  bcryptjs  from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 export const test = (req, res) => {
   res.json({
     message: "Hello world",
   });
 };
-export const updateUser = async(req, res, next) => {
+export const updateUser = async (req, res, next) => {
   if (req.user.id != req.params.id) {
     return next(errorHandler(401, "forbidden"));
   }
   try {
-    if(req.body.password){
-        req.body.password = bcryptjs.hashSync(req.body.password,10);
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-    const updateUser = await User.findByIdAndUpdate(req.params.id,{
-        $set:{
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-            avatar:req.body.avatar,
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
         },
-    },{new:true})
+      },
+      { new: true }
+    );
 
-    const {password,...rest} = updateUser._doc;
-    res.status(200).json(rest)
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
   } catch (err) {
     next(err);
   }
 };
-export const deleteUser = async (req,res,next) =>{
-    if(req.user.id !== req.params.id){
-        return next(errorHandler(401,'you can only update your own id '))
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "you can only update your own id "));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json({
+      message: "user is succesfully deleted",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listing = await Listing.find({useRef:req.params.id})
+      res.status(200).json(listing);
+    } catch (err) {
+      next(err);
     }
-    try{
-        await User.findByIdAndDelete(req.params.id)
-        res.clearCookie('access_token');
-        res.status(200).json({
-            message:"user is succesfully deleted"
-        })
-    }
-    catch(err){
-        next(err);
-    }
-    
-}
+  } else {
+    next(errorHandler(401, "You can only view your own listing"));
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {deleteUser, test, updateUser} from '../controllers/user.controller.js'
+import {deleteUser, getUserListing, test, updateUser} from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verfiyUser.js';
 const router = express.Router();
 router.get('/test',test);
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
+router.get('/listing/:id',verifyToken,getUserListing);
 export default router

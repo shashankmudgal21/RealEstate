ef93281a4cf93d2159ff7ca7335633b94df0f42f
Shashank mudgal <=>
2023-11-23 23:29:51 +0530

    complted backend and frontend both

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,9 +5,9 @@ import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashPassword });
+  const newUer = new User({ username, email, password: hashPassword });
   try {
-    await newUer.save();
+    await newUser.save();
     res.status(201).json({
       message: "User creates succesfully",
     });
